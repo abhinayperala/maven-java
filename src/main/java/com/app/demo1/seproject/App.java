45df63942e59a3d680394ff4316fac1ac824afcf
unknown <abhinay.perala@gmail.com>
2023-12-05 10:09:22 +0530

    Demo for Team

---
+++ src/main/java/com/app/demo1/seproject/App.java
@@ -0,0 +1,13 @@
+package com.app.demo1.seproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
